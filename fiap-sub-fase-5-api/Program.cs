b8f1b3b35d642c4a104499e49547a9de9bad54fa
Felipe Gonçalves <lipegn2@gmail.com>
2025-08-03 09:58:53 -0300

    Correção rotas

--- fiap-sub-fase-5-api/Program.cs
+++ fiap-sub-fase-5-api/Program.cs
@@ -1,20 +1,34 @@
+using fiap_sub_fase_5_api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// (opcional) sempre gerar endpoints do Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Habilite Swagger também em produção (ou condicione por variável de ambiente)
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseAuthorization();
+// Redireciona a raiz para o Swagger
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
-app.MapControllers();
+// Endpoint padrão do template continua:
+app.MapGet("/weatherforecast", () =>
+{
+    var summaries = new[] { "Freezing","Bracing","Chilly","Cool","Mild",
+                            "Warm","Balmy","Hot","Sweltering","Scorching" };
+    return Enumerable.Range(1, 5).Select(index =>
+        new PrevisaoTempo
+        {
+            Cidade = "Cidade",
+            Data = DateTime.Now,
+            TemperaturaC = 10,
+            Resumo = "ObterResumo(temperaturaC)"
+        }
+        ).ToArray();
+});
 
 app.Run();
